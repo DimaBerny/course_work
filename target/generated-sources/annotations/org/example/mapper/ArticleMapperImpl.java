7e9e5ae5af70c1d318f33675533d9582776cb5a2
unknown <berny2611@ukr.net>
2023-11-28 21:02:39 +0200

    Initial comit

---
+++ target/generated-sources/annotations/org/example/mapper/ArticleMapperImpl.java
@@ -0,0 +1,13 @@
+package org.example.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-11-28T20:46:24+0200",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.9 (Amazon.com Inc.)"
+)
+@Component
+public class ArticleMapperImpl implements ArticleMapper {
+}
